ab5983ff1d0bc73bd478daa54dfeb5b5d9a154f8
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2025-01-17 11:25:36 +0600

    feat: add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Hero from '../components/home/Hero';
+import Specials from '../components/home/Specials';
+import Testimonials from '../components/home/Testimonials';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Specials />
+      <Testimonials />
+    </>
+  );
+}
+
+export default Home;
